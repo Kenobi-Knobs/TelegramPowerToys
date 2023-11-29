c98a7d0a22753431783574b923ff01557740f0c3
Konchatniy Vitaliy <konchatniyvitaliy@gmail.com>
2023-11-29 18:59:22 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+﻿const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	const nodesPath = path.join(__dirname, './data/nodes.json');
+	const edgesPath = path.join(__dirname, './data/edges.json');
+	const rawNodesData = fs.readFileSync(nodesPath);
+	const rawEdgesData = fs.readFileSync(edgesPath);
+
+	const nodesData = JSON.parse(rawNodesData);
+	const edgesData = JSON.parse(rawEdgesData);
+
+	res.send({nodes: nodesData, edges: edgesData});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
