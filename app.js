39e3b27a14cc45979d8f3c9404d34bb970d018ff
Konchatniy Vitaliy <konchatniyvitaliy@gmail.com>
2024-02-27 16:32:57 +0200

    fix files creation, rename edges

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 const port = 3000;
 
 app.set('view engine', 'ejs');
@@ -9,18 +10,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	const nodesPath = path.join(__dirname, './data/nodes.json');
-	const edgesPath = path.join(__dirname, './data/edges.json');
-	const networkPath = path.join(__dirname, './data/network.json');
+	const nodesPath = path.join(__dirname, config.dataFolder, 'nodes.json');
+	const connectionsPath = path.join(__dirname, config.dataFolder, 'connections.json');
+	const networkPath = path.join(__dirname, config.dataFolder, 'network.json');
 	const rawNodesData = fs.readFileSync(nodesPath);
-	const rawEdgesData = fs.readFileSync(edgesPath);
+	const rawConnectionsData = fs.readFileSync(connectionsPath);
 	const rawNetworkData = fs.readFileSync(networkPath);
 
 	const nodesData = JSON.parse(rawNodesData);
-	const edgesData = JSON.parse(rawEdgesData);
+	const connectionsData = JSON.parse(rawConnectionsData);
 	const networkData = JSON.parse(rawNetworkData);
 
-	res.render('index', { nodes: nodesData, edges: edgesData, network: networkData});
+	res.render('index', { nodes: nodesData, connections: connectionsData, network: networkData});
 });
 
 app.listen(port, () => {
--- etl/load.js
+++ etl/load.js
@@ -10,14 +10,15 @@ class Load {
 	}
 
 	async execute() {
+		// create data folder if not exists
 		if (!fs.existsSync(this.config.dataFolder)) {
 			fs.mkdirSync(this.config.dataFolder);
 		}
 
-		fs.writeFileSync(this.config.nodesDataPath, JSON.stringify(this.nodes), 'utf8');
-		fs.writeFileSync(this.config.connectionsDataPath, JSON.stringify(this.connections), 'utf8');
-		fs.writeFileSync(this.config.postsDataPath, JSON.stringify(this.posts), 'utf8');
-		fs.writeFileSync(this.config.networkDataPath, JSON.stringify(this.network), 'utf8');
+		fs.writeFileSync(this.config.dataFolder + '/nodes.json', JSON.stringify(this.nodes), 'utf8');
+		fs.writeFileSync(this.config.dataFolder + '/connections.json', JSON.stringify(this.connections), 'utf8');
+		fs.writeFileSync(this.config.dataFolder + '/posts.json', JSON.stringify(this.posts), 'utf8');
+		fs.writeFileSync(this.config.dataFolder + '/network.json', JSON.stringify(this.network), 'utf8');
 	}
 }
 
